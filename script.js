d934e911608ed9eb3ecdce34a6fd93a20eb0481c
Dimitar Ivanov <104774295+dimnov@users.noreply.github.com>
2022-09-09 23:30:05 +0300

    Password Generator

---
+++ script.js
@@ -0,0 +1,112 @@
+const pwEl = document.getElementById("password");
+const copyEl = document.getElementById("copy-item");
+const lenEl = document.getElementById("length");
+const upperEl = document.getElementById("uppercase");
+const lowerEl = document.getElementById("lowercase");
+const numberEl = document.getElementById("number");
+const symbolEl = document.getElementById("symbol");
+const extraEl = document.getElementById("extra");
+const generateEl = document.getElementById("gen-pass");
+
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0123456789";
+const symbols = "!@#$%^&*";
+const extra = "~`()_-+={[}]|:;'<,>.?/ ";
+
+function getLowercase() {
+  return lowerLetters[Math.floor(Math.random() * lowerLetters.length)];
+}
+
+function getUppercase() {
+  return upperLetters[Math.floor(Math.random() * upperLetters.length)];
+}
+
+function getNumber() {
+  return numbers[Math.floor(Math.random() * numbers.length)];
+}
+
+function getSymbol() {
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+function getExtra() {
+  return extra[Math.floor(Math.random() * extra.length)];
+}
+
+function generatePassword() {
+  const len = lenEl.value;
+
+  let password = "";
+
+  if (upperEl.checked) {
+    password += getUppercase();
+  }
+
+  if (lowerEl.checked) {
+    password += getLowercase();
+  }
+
+  if (numberEl.checked) {
+    password += getNumber();
+  }
+
+  if (symbolEl.checked) {
+    password += getSymbol();
+  }
+
+  if (extraEl.checked) {
+    password += getExtra();
+  }
+
+  for (let i = password.length; i < len; i++) {
+    const x = generateX();
+    password += x;
+  }
+
+  pwEl.innerText = password;
+}
+
+function generateX() {
+  const xs = [];
+  if (upperEl.checked) {
+    xs.push(getUppercase());
+  }
+
+  if (lowerEl.checked) {
+    xs.push(getLowercase());
+  }
+
+  if (numberEl.checked) {
+    xs.push(getNumber());
+  }
+
+  if (symbolEl.checked) {
+    xs.push(getSymbol());
+  }
+
+  if (extraEl.checked) {
+    xs.push(getExtra());
+  }
+
+  if (xs.length === 0) return "";
+
+  return xs[Math.floor(Math.random() * xs.length)];
+}
+
+generateEl.addEventListener("click", generatePassword);
+
+copyEl.addEventListener("click", () => {
+  const textarea = document.createElement("textarea");
+  const password = pwEl.innerText;
+
+  if (!password) {
+    return;
+  }
+
+  textarea.value = password;
+  document.body.appendChild(textarea);
+  textarea.select();
+  document.execCommand("copy");
+  textarea.remove();
+});
